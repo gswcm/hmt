d226b9b87396a5ca61212f5553fab077ce82e866
gswcm <simonbaev@gmail.com>
2018-02-06 15:58:03 -0500

    io -> socket.broadcast

--- backend/lib/socket.io/scantron.js
+++ backend/lib/socket.io/scantron.js
@@ -2,7 +2,7 @@ module.exports = (io) => {
 	io.on('connect', socket => {
 		console.log(`Connected to ${socket.id}`);		
 		socket.on('scanData', data => {			
-			io.emit('addScanData', data);
+			socket.broadcast.emit('addScanData', data);
 		});
 	});
 };
